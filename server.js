cba9c22939d18d917ecffe435fd6df19c127033b
Boshra Jaber <boshrajaber94@gmail.com>
2021-03-28 17:57:12 +0300

    test lint

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const app = express();
 const notFountHandler = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
 
-app.get('/', (req,res)=>{
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 // create a route for errors:
-app.get('/error', (req,res)=>{
-    throw new Error('Something went wrong!')
+app.get('/error', (req, res) => {
+  throw new Error('Something went wrong!');
 });
 // these must be after all of the routes
 app.use('*', notFountHandler);
 app.use(errorHandler);
 
-function start(port){
-    app.listen(port, ()=>{
-        console.log(`Server is up on ${port}`);
-    })
+function start(port) {
+  app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+  });
 }
 // to use this outside the file we export what we need
 // exports is a property inside the module object
 module.exports = {
-    app: app,
-    start : start,
-}
+  app: app,
+  start: start,
+};
